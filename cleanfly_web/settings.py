662faefd190844623aa846f11926d6ccca196dd0
root <root@ip-172-31-25-84.ap-northeast-1.compute.internal>
2016-07-09 10:12:22 +0000

    remove useless middleware!

--- cleanfly_web/settings.py
+++ cleanfly_web/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = ['easy.getcleanfly.com','localhost']
 # Application definition
 
 INSTALLED_APPS = (
-    'sslserver',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
